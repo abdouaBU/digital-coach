9f2a99b0daa40cd96fefb96fb872f45c3c504186
EthanWesterburg <ethanlw2017@gmail.com>
2026-03-26 11:28:24 -0400

    implemented UserInputData mostly

--- demo/src/main/java/UserInputData.java
+++ demo/src/main/java/UserInputData.java
@@ -1,3 +1,53 @@
+import java.util.*;
+
 public class UserInputData {
-    
+    private UserGoalEnum userGoal;
+    private UserLevelEnum userLevel;
+    private int numDays;
+    private String[] targetMuscles;
+    private Set<String> availableEquipment;
+
+
+    public enum UserGoalEnum {
+        STRENGTH,
+        MUSCLE,
+        BOTH;
+    }
+
+    public enum UserLevelEnum {
+        BEGINNER,
+        INTERMEDIATE,
+        ADVANCED;
+    }
+
+    //make constructor when I know how to get the targetMuscles and availableEquipment
+    //information from other people
+    public UserInputData(){
+        //TODO
+    }
+
+    //dont need setters cause object doesn't need to be mutable
+
+
+    //Getters
+    public UserGoalEnum getUserGoal(){
+        return this.userGoal;
+    }
+
+    public UserLevelEnum getUserLevel(){
+        return this.userLevel;
+    }
+
+    public int getNumDays(){
+        return this.numDays;
+    }
+
+    public String[] getTargetMuscles(){
+        return this.targetMuscles;
+    }
+
+    public Set<String> getAvailableEquipment(){
+        return this.availableEquipment;
+    }
+
 }
