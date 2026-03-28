c0821930714e9a4ed9f0f26fc9b44f750db9766e
Daniel George <diggyzar@bu.edu>
2026-03-27 23:02:27 -0400

    set up image recognition api and "detect' endpoint

--- backend/src/main/java/com/example/demo/ImageRecognitionService.java
+++ backend/src/main/java/com/example/demo/ImageRecognitionService.java
@@ -1,5 +1,32 @@
 package com.example.demo;
 
+import com.google.cloud.spring.vision.CloudVisionTemplate;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.Feature.Type;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service // This tells Spring to manage this class
 public class ImageRecognitionService {
-    // image recognition service
+
+    @Autowired
+    private CloudVisionTemplate cloudVisionTemplate;
+
+    /**
+     * Identifies objects/labels from an uploaded file
+     */
+    public List<String> getLabels(MultipartFile file) {
+        
+        AnnotateImageResponse response = this.cloudVisionTemplate.analyzeImage(
+                file.getResource(), 
+                Type.LABEL_DETECTION);
+
+        return response.getLabelAnnotationsList().stream()
+                .map(label -> label.getDescription())
+                .collect(Collectors.toList());
+    }
 }
--- backend/src/main/java/com/example/demo/WorkoutRecommendationHandler.java
+++ backend/src/main/java/com/example/demo/WorkoutRecommendationHandler.java
@@ -1,14 +1,22 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 // import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:5173") // React dev server
 public class WorkoutRecommendationHandler {
+
+    @Autowired
+    private ImageRecognitionService imageRecognitionService;
 
     // POST /api/workout — receives JSON body and maps it to UserInputData
     @PostMapping("/workout")
@@ -21,11 +29,6 @@ public class WorkoutRecommendationHandler {
         System.out.println("  Equipment:  " + userInput.getAvailableEquipment());
 
         //TODO
-        //this is danny's job to get an equipment list (technically java set)
-        // Set<String> equipment = some method in ImageRecognitionService
-        //setAvailableEquipment(equipment);
-
-        //TODO
         //this is ethan's job to generate the workout
         WorkoutSchedule workoutSchedule = generateWorkout(userInput);
 
@@ -33,6 +36,27 @@ public class WorkoutRecommendationHandler {
         //will return a WorkoutSchedule object and jackson will automatically convert it to json
         //then front end will need to figure out how to display it
         return "Workout config received for goal: " + userInput.getUserGoal();
+    }
+
+    // get an equipment list (technically java set)
+    // Set<String> equipment = some method in ImageRecognitionService
+    //setAvailableEquipment(equipment);
+    @PostMapping("/detect")
+    public Set<String> getEquipment(@RequestParam("files") MultipartFile[] files) {
+        Set<String> allDetectedEquipment = new HashSet<>();
+        System.out.println("Received " + files.length + " files.");
+
+        try {
+            for (MultipartFile file : files) {
+                List<String> labels = this.imageRecognitionService.getLabels(file);
+                allDetectedEquipment.addAll(labels); 
+            }
+            return allDetectedEquipment;
+        } catch (Exception e) {
+            System.err.println("VISION API FAILED: " + e.getMessage());
+            e.printStackTrace();
+            return null; 
+        }
     }
 
     public WorkoutSchedule generateWorkout(UserInputData userdata){
